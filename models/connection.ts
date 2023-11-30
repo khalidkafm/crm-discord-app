84cf1695609e6b572b0299f37f3524e48917e322
Arthurontime <local-git.squiggle185@passmail.net>
2023-11-30 17:24:43 +0100

    test with connectionString in file

--- models/connection.ts
+++ models/connection.ts
@@ -1,6 +1,7 @@
 const mongodb = require("mongoose");
 
-const connectionString = process.env.MONGO_DB_CONNEXION
+//const connectionString = process.env.MONGO_DB_CONNEXION
+const connectionString = "mongodb+srv://crm_discord_app:Plod7-Undercoat6-Landed9-Overexert4-Bronzing2-Spooky7-Ragweed8-Persevere0-Bungee5-Hanky8@lacapsule.hssychg.mongodb.net/crmdiscord"
 
 // handle connection
 mongodb.connect( connectionString, {connectTimeoutMS:2000})
--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 
 require ('./models/connection.ts')
 
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    MONGO_DB_CONNEXION: 'mongodb+srv://crm_discord_app:Plod7-Undercoat6-Landed9-Overexert4-Bronzing2-Spooky7-Ragweed8-Persevere0-Bungee5-Hanky8@lacapsule.hssychg.mongodb.net/crmdiscord',
+  },
+}
   
   module.exports = nextConfig;
   
