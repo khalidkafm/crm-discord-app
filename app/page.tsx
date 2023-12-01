477e97f4a6ade1714501e5a5b93cf53be171a350
Arthurontime <local-git.squiggle185@passmail.net>
2023-12-01 07:43:19 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ const Page = async () => {
 		<>
 			<h1>Profile</h1>
 			<p>User id: {session.user.userId}</p>
-			<p>Username: { username }</p>
+			<p>Username is: { username }</p>
 			<Form action="/api/logout">
 				<input type="submit" value="Sign out" />
 			</Form>
