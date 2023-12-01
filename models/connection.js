aec1d2cc91ce1e5c191981d282d25511d7879baa
Arthurontime <local-git.squiggle185@passmail.net>
2023-12-01 11:48:25 +0100

    hard coded DB connection String

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,8 @@
 //const mongodb = require("mongoose");
 import mongodb from 'mongoose';
 
-const connectionString = process.env.MONGO_DB_CONNEXION;
+//const connectionString = process.env.MONGO_DB_CONNEXION;
+const connectionString = "mongodb+srv://crm_discord_app:Plod7-Undercoat6-Landed9-Overexert4-Bronzing2-Spooky7-Ragweed8-Persevere0-Bungee5-Hanky8@lacapsule.hssychg.mongodb.net/crmdiscord"
 
 // handle connection
 // ready states being: 0: disconnected - 1: connected - 2: connecting - 3: disconnecting
