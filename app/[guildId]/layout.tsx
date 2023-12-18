dbc797599e27cfcc9b3ff2cf28856a132ed66ffa
Marc <marcnhallan@gmail.com>
2023-12-18 10:13:52 +0100

    sidebarmenu fixes, inviteList, accountCard, scrollmenu, filterDta

--- app/[guildId]/layout.tsx
+++ app/[guildId]/layout.tsx
@@ -8,33 +8,43 @@ import SideBarMenu from "@/components/sidebar-menu";
 import { User } from "../../models/user";
 import { Message } from "../../models/message";
 
-
+interface GuildIdParams {
+  guildId: string;
+  memberId: string;
+}
 
 export default async function GuildId({
   params,
   children,
 }: {
-  params: { guildId: string };
+  params: GuildIdParams;
   children: React.ReactNode;
 }) {
-  const authRequest = auth.handleRequest("GET", context);
+  const authRequest:any = auth.handleRequest("GET", context);
   const session = await authRequest.validate();
 
   if (!session) redirect("/login");
+  
 
   // Fetch messages from MongoDB
   let messages: any;
 
-  const { username } = await User.findOne({ _id: session.user.userId }).exec();
+  const { username, discordId } = await User.findOne({ _id: session.user.userId }).exec();
   await Message.find().then((data: any) => {
     messages = data;
   });
+
+const memberId = discordId;
+console.log(memberId);
+
+ 
+
   return (
     <>
         <WarningHeader></WarningHeader>
         <div className="flex h-screen">
         <div className="w-1/4 bg-gray-75 border-r-2 border-solid border-gray-500">
-            <SideBarMenu guildId={params.guildId}></SideBarMenu>
+            <SideBarMenu guildId={params.guildId} memberId={memberId} sessionData={{ data: { memberId}, status: 'authenticated' }}></SideBarMenu>
         </div>
         <div className="flex-1 p-4 overflow-hidden">
             {children}
