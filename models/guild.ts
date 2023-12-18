b1a5bb08a2f2c3347b44e286cb947250886c73e8
khalid <khalid91e@hotmail.fr>
2023-12-18 09:41:10 +0100

    popover and invites.ts

---
+++ models/guild.ts
@@ -0,0 +1,31 @@
+import mongodb from "mongoose";
+
+export const Guild = mongodb.models.Guild || mongodb.model("Guild",
+new mongodb.Schema(
+	{
+        discordId: {
+			type: String,
+			required: true
+		},
+		name: {
+			type: String,
+			required: true
+		},
+		icon: {
+			type: String,
+			required: false
+		},
+		permissions: {
+			type: String,
+			required: false
+		}, 
+		ownerId: {
+			type: String,
+			required: false
+		},
+        joinedTimestamp: {
+			type: Number,
+			required: true
+		},
+	}
+));
