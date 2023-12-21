458b840de4e1ef9565fe4b66ca336c864baf30c0
Marc <marcnhallan@gmail.com>
2023-12-21 17:31:19 +0100

    fixing login page style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,9 @@ import { auth } from "@/auth/lucia";
 import * as context from "next/headers";
 import { redirect } from "next/navigation";
 
-import { Metadata } from "next"
-import Link from "next/link"
+import { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -26,7 +27,7 @@ const Page = async () => {
 	if (session) redirect("/");
 	return (
 		<>
-		<div className="container relative h-[800px] flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+		<div className="container absolute h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
 		  <Link
 			href="/login/discord"
 			className={cn(
@@ -36,32 +37,19 @@ const Page = async () => {
 		  >
 			Login
 		  </Link>
-		  <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-			<div className="absolute inset-0 bg-zinc-900" />
-			<div className="relative z-20 flex items-center text-lg font-medium">
-			  <svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				className="mr-2 h-6 w-6"
-			  >
-				<path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-			  </svg>
-			  Acme Inc
+		  <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex justify-center items-center">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex flex-col items-center justify-center text-lg font-medium h-full">
+         <Image
+          src="/LogoDiscordCRM.png"
+          alt=""
+          width={400}
+          height={140}
+          className="relative top-15 px-5 py-5"
+        ></Image>
+
 			</div>
 			<div className="relative z-20 mt-auto">
-			  <blockquote className="space-y-2">
-				<p className="text-lg">
-				  &ldquo;This library has saved me countless hours of work and
-				  helped me deliver stunning designs to my clients faster than
-				  ever before.&rdquo;
-				</p>
-				<footer className="text-sm">Sofia Davis</footer>
-			  </blockquote>
 			</div>
 		  </div>
 		  <div className="lg:p-8">
